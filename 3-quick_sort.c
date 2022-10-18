4835a613a7da94ff6310dd039e57f4a4d356a8e3
n1klaus <nicknyanjui@gmail.com>
2022-10-18 18:37:12 +0300

    Update 3-quick_sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,55 +1,60 @@
 #include "sort.h"
 /**
- * swap - swap two elements @i and @j in an @a
- * @a : array of elements
- * @i : integer number
- * @j : integer number
+ * swap - swap values of two elements @i and @j
+ * @i : pointer to an integer number
+ * @j : pointer to an integer number
  *
  * Return : Nothing
  */
-void swap(int a[], int i, int j)
+void swap(int *i, int *j)
 {
-	int temp = 0;
+	int temp = *i;
+	*i = *j;
+	*j = temp;
+}
 
-	temp = a[i];
-	a[i] = a[j];
-	a[j] = temp;
+/**
+ * get_partition_index - Get the partition index
+ * @array : array of elements
+ * @low : left most index position
+ * @high : right most index position
+ * @size : size of the array
+ *
+ * Return: position of the pivot in the @array
+ */
+size_t get_partition_index(int *array, int low, int high, size_t size)
+{
+	int index = low, pivot = array[high], pi = low - 1;
+
+	for (; index < high; index++)
+		if (array[index] < pivot)
+			swap(&array[++pi], &array[index]);
+
+	swap(&array[++pi], &array[high]);
+	print_array(array, size);
+	return (pi);
 }
 
 /**
  * partition - partition @array of @size into sub-arrays
- * using @left as lower bound and @right as higher bound
+ * using @low as lower bound and @high as higher bound
  * @array : array of elements
- * @left : left most index position
- * @right : right most index position
+ * @low : left most index position
+ * @high : right most index position
  * @size : size of the array
  *
  * Return : Nothing
  */
-void partition(int *array, int left, int right, int size)
+void partition(int *array, int low, int high, size_t size)
 {
-	int NOTSORTED = 1, pivot, mid, low = left - 1, high = right + 1;
+	int pi = 0;
 
-	if (left >= right)
-		return;
-	pivot = array[(left + right) / 2];
-
-	while (NOTSORTED)
+	if (low < high)
 	{
-		do {
-			low++;
-		} while (array[low] < pivot);
-		do {
-			high--;
-		} while (array[high] > pivot);
-		if (low >= high)
-			break;
-		swap(array, low, high);
-		print_array(array, size);
+		pi = get_partition_index(array, low, high, size);
+		partition(array, low, pi - 1, size);
+		partition(array, pi + 1, high, size);
 	}
-	mid = high;
-	partition(array, mid + 1, right, size);
-	partition(array, left, mid, size);
 }
 
 /**
@@ -66,7 +71,6 @@ void partition(int *array, int left, int right, int size)
  */
 void quick_sort(int *array, size_t size)
 {
-	int left = 0, right = size - 1;
 	char *best_case = "O(nlogn)", *average_case = "O(nlogn)";
 	char *worst_case = "O(n^2)";
 	FILE *fp = fopen("3-O", "w");
@@ -76,7 +80,7 @@ void quick_sort(int *array, size_t size)
 		fclose(fp);
 		exit(EXIT_FAILURE);
 	}
-	partition(array, left, right, size);
+	partition(array, 0, size - 1, size);
 
 	fprintf(fp, "%s\n%s\n%s\n", best_case, average_case,
 		worst_case);
