d778c209faf42d7bc6ecac30cebb01279b23fa5f
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-02 18:21:13 +0300

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -28,38 +28,38 @@ size_t max(int *array, size_t size)
  */
 void counting_sort(int *array, size_t size)
 {
-	size_t index = 0, counter = 0, range = (max(array, size) + 1);
+	size_t index = 0, counter = 0, loc = 0, range = (max(array, size) + 1);
 	char *best_case = "O(nlogn)", *average_case = "O(n)";
 	char *worst_case = "O(n)";
 	int *count = (int *)malloc(sizeof(int) * range), count_ref = 0;
 	int *output = (int *)malloc(sizeof(int) * size);
 	FILE *fp = fopen("102-O", "w");
 
-	if (fp == NULL || array == NULL)
+	if (fp && size > 0)
 	{
-		fclose(fp);
-		exit(EXIT_FAILURE);
-	}
-	for (index = 0; index < range; index++)
-		count[index] = 0;
-	for (index = 0; index < size; ++index)
-	{
-		if (index < size - 1 && array[index] > array[index + 1])
-			count_ref += 1;
-		counter = array[index];
-		++count[counter];
-	}
-	if (count_ref > 0)
-	{
+		for (index = 0; index < range; index++)
+			count[index] = 0;
+
+		for (index = 0; index < size; ++index)
+		{
+			if (index < size - 1 && array[index] > array[index + 1])
+				count_ref += 1;
+			counter = array[index];
+			++count[counter];
+		}
+
 		for (index = 1; index < range; index++)
 			count[index] += count[index - 1];
+
 		print_array(count, range);
 		for (index = size - 1; (int)index >= 0; --index)
 		{
 			counter = array[index];
-			output[count[counter] - 1] = array[index];
+			loc = count[counter];
+			output[loc - 1] = array[index];
 			--count[counter];
 		}
+
 		for (index = 0; index < size; ++index)
 			array[index] = output[index];
 	}
