ad0d8cd5020449437836f9aa26c01b4caddaff54
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-02 18:09:36 +0300

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,5 +1,22 @@
 #include "sort.h"
 /**
+ * max - get the maximum number in the array
+ * @array: array of elements
+ * @size: size of the array
+ *
+ * Return: maximum number in a list of numbers
+ */
+size_t max(int *array, size_t size)
+{
+	size_t index = 0, max = 0;
+
+	for (; index < size; index++)
+		if (array[index] > (int)max)
+			max = array[index];
+	return (max);
+}
+
+/**
  * counting_sort - integer sorting algorithm for a collection of objects
  * according to keys as index to @array by counting number of objects
  * with distinct keys and applying prefix sum on those counts to determine
@@ -11,10 +28,10 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	size_t index = 0, counter = 0, loc = 0, range = 100;
+	size_t index = 0, counter = 0, range = (max(array, size) + 1);
 	char *best_case = "O(nlogn)", *average_case = "O(n)";
 	char *worst_case = "O(n)";
-	int *count = (int *)malloc(sizeof(int) * range);
+	int *count = (int *)malloc(sizeof(int) * range), count_ref = 0;
 	int *output = (int *)malloc(sizeof(int) * size);
 	FILE *fp = fopen("102-O", "w");
 
@@ -23,29 +40,29 @@ void counting_sort(int *array, size_t size)
 		fclose(fp);
 		exit(EXIT_FAILURE);
 	}
-
 	for (index = 0; index < range; index++)
 		count[index] = 0;
 	for (index = 0; index < size; ++index)
 	{
+		if (index < size - 1 && array[index] > array[index + 1])
+			count_ref += 1;
 		counter = array[index];
 		++count[counter];
 	}
-
-	for (index = 1; index < range; index++)
-		count[index] += count[index - 1];
-	print_array(count, range);
-
-	for (index = size - 1; (int)index >= 0; --index)
+	if (count_ref > 0)
 	{
-		counter = array[index];
-		loc = count[counter];
-		output[loc - 1] = array[index];
-		--count[counter];
+		for (index = 1; index < range; index++)
+			count[index] += count[index - 1];
+		print_array(count, range);
+		for (index = size - 1; (int)index >= 0; --index)
+		{
+			counter = array[index];
+			output[count[counter] - 1] = array[index];
+			--count[counter];
+		}
+		for (index = 0; index < size; ++index)
+			array[index] = output[index];
 	}
-	for (index = 0; index < size; ++index)
-		array[index] = output[index];
-
 	fprintf(fp, "%s\n%s\n%s\n", best_case, average_case,
 		worst_case);
 	free(count);
